11351f4b6e127c3f82a46e62765a89b8fa9f678d
father-gregor <benlinus92@gmail.com>
2017-05-20 23:26:37 +0300

    Add separate file for twitch functions

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
+'use strict';
+
 var express = require("express");
 var app = express();
+var twitch = require("./src/server/twitch.js");
 
+app.set("port", (process.env.PORT || 5000));
+app.use(express.static(__dirname + "/src/public"));
 app.get("/", function(req, res) {
-  res.send("Hello World");
+  res.sendFile(__dirname + "/src/views/index.html");
+});
+app.get("/get-stream", function(req, res) {
+  var streamName = "stream";
+  console.log("In Server.js");
+  twitch.getStream(streamName);	
 });
 
-app.listen(3000);
+app.listen(app.get("port"), function() {
+	console.log("Server started on port " + app.get("port"));
+});
---
+++ src/server/twitch.js
@@ -0,0 +1,8 @@
+'use strict';
+var base_USHER = "https://usher.ttvnw.net/api/channel/hls/"
+function getStream(name) {
+	console.log("Received - " + name);
+}
+module.exports = {
+	getStream
+}
